4e9eb11ae552ee5ee5419c6680d007046ab64983
Austen Allred <austen.allred@gmail.com>
2015-01-31 17:16:32 -0700

    Initial Commit

---
+++ Weekly Reminders/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Weekly Reminders
+//
+//  Created by Austen Allred on 1/31/15.
+//  Copyright (c) 2015 Austen Allred. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
