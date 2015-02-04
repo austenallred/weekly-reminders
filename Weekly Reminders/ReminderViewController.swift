bdbe13c6dcadbea391ca94124412a5f2cfc116ba
Austen Allred <austen.allred@gmail.com>
2015-02-04 00:57:00 -0700

    add reminder controller

---
+++ Weekly Reminders/ReminderViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ReminderViewController.swift
+//  Weekly Reminders
+//
+//  Created by Austen Allred on 2/4/15.
+//  Copyright (c) 2015 Austen Allred. All rights reserved.
+//
+
+import UIKit
+
+class ReminderViewController: UIViewController {
+
+    @IBOutlet var goalRemindLabel: UILabel!
+    override func viewDidLoad() {
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
