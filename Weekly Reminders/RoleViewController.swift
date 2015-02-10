214dc247efda85ac4711e17457a6edf1f9dee415
Austen Allred <austen.allred@gmail.com>
2015-02-09 18:20:44 -0700

    embed roles in table view controller

--- Weekly Reminders/RoleViewController.swift
+++ Weekly Reminders/RoleViewController.swift
@@ -8,19 +8,50 @@
 
 import UIKit
 
-class RoleViewController: UIViewController {
+class RoleViewController: UITableViewController, UITextFieldDelegate {
  
     @IBOutlet var roleOneField: UITextField!
     @IBOutlet var roleTwoField: UITextField!
     @IBOutlet var roleThreeField: UITextField!
     @IBOutlet var roleFourField: UITextField!
-    
+        
     @IBAction func submitRolesButton() {
         NSUserDefaults.standardUserDefaults().setObject(roleOneField.text, forKey: "roleOne")
         NSUserDefaults.standardUserDefaults().setObject(roleTwoField.text, forKey: "roleTwo")
         NSUserDefaults.standardUserDefaults().setObject(roleThreeField.text, forKey: "roleThree")
         NSUserDefaults.standardUserDefaults().setObject(roleFourField.text, forKey: "roleFour")
         NSUserDefaults.standardUserDefaults().synchronize()
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+            
+    }
+    
+
+    func textFieldShouldBeginEditing(textField: UITextField) -> Bool {
+        
+        return true
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+    
+        if textField == roleOneField {
+            roleTwoField.becomeFirstResponder()
+        }
+
+        if textField == roleTwoField {
+            roleThreeField.becomeFirstResponder()
+        }
+
+        if textField == roleThreeField {
+            roleFourField.becomeFirstResponder()
+        }
+    
+        if textField == roleFourField {
+            textField.resignFirstResponder()
+        }
+        
+        return true
     }
     
 }
